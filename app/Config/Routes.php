b7752ffc938cbd4cd02982ba8180bdd83a5ae8d1
João Vitor Xavier de Carvalho <jaovitorxc@gmail.com>
2025-09-17 22:57:34 -0300

    inseri um novo controller e model pra atribuir produtos aos vendedores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,6 +10,9 @@ $routes->group('src', function (RouteCollection $routes){
     $routes->post('login', 'LoginController::logarUsuario');
     $routes->post('carrinho', 'CarrinhoController::carrinho');
     $routes->post('pagamento', 'PagamentoController::gerarTransacao');
+    $routes->post('usuario-produto/atribuir', 'UsuarioProdutoController::atribuirProduto');
+    $routes->get('usuario-produto/listar/(:num)', 'UsuarioProdutoController::listarProdutos/$1');
+    $routes->post('usuario-produto/remover', 'UsuarioProdutoController::removerProduto');
 });
 
 $routes->resource('produtos', ['controller' => 'ProdutosController']);
