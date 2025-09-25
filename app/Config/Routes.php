0a6313d795d1727ae623535e3cff85927f504a7d
MiguelSommerf <miguel.sommerfeldluiz@gmail.com>
2025-09-25 01:49:41 -0300

    Corrigindo as rotas e criando novos grupos.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,21 +6,26 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->group('src', function (RouteCollection $routes){
-    $routes->post('cadastrar', 'LoginController::cadastrarUsuario');
-    $routes->post('login', 'LoginController::logarUsuario');
-    $routes->post('carrinho', 'CarrinhoController::carrinho');
-    $routes->post('pagamento', 'PagamentoController::gerarTransacao');
+    $routes->group('usuario', function (RouteCollection $routes) {
+        $routes->post('cadastrar', 'LoginController::cadastrarUsuario');
+        $routes->post('login', 'LoginController::logarUsuario');
+        $routes->post('carrinho', 'CarrinhoController::carrinho');
+        $routes->post('pagamento', 'PagamentoController::gerarTransacao');
+    });
+
+    $routes->group('vendedor', function (RouteCollection $routes) {
+        $routes->post('cadastrar', 'VendedorController::cadastrarVendedor');
+        $routes->post('login', 'VendedorController::logarVendedor');
+    });
 
     $routes->group('produtos', function (RouteCollection $routes) {
-    $routes->get('selecionarProduto/(:num)', 'ProdutosController::selecionarProduto/$1');
+        $routes->get('selecionarProduto/(:num)', 'ProdutosController::selecionarProduto/$1');
+    });
 
-    $routes->group('usuario-produto', function (RouteCollection $routes) {
-    $routes->post('', 'UsuarioProdutoController::atribuirProduto');
-    $routes->get('(:num)', 'UsuarioProdutoController::listarProdutos/$1');
-    $routes->delete('', 'UsuarioProdutoController::removerProduto');
-});
-
-        
+    $routes->group('vendedor/produto', function (RouteCollection $routes) {
+        $routes->post('atribuir', 'VendedorProdutoController::atribuirProduto');
+        $routes->get('listar/(:num)', 'VendedorProdutoController::listarProdutos/$1');
+        $routes->delete('deletar', 'VendedorProdutoController::removerProduto');
     });
 });
 
