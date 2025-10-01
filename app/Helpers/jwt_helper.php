a9355932230abe7788649db13e823159e016494d
Enzo Móbile de Oliveira <prenzo0001@gmail.com>
2025-10-01 00:04:25 -0300

    Atualização do firebase/php-jwt de v5 para v6.11.1, pequena melhoria no jwtHelper.

--- app/Helpers/jwt_helper.php
+++ app/Helpers/jwt_helper.php
@@ -3,23 +3,29 @@
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-function createJWT($user): Firebase\JWT\JWT|string
+function createJWT(array $user): Firebase\JWT\JWT|string
 {
+    if (empty($user['id']) || empty($user['email'])) {
+        throw new InvalidArgumentException('Dados do usuário incompletos');
+    }
+
     $issuedAt = time();
-    $expire = $issuedAt + getenv('jwt.expiration');
+    $expire = $issuedAt + (int)getenv('jwt.expiration');
     $payload = [
         'iss' => getenv('jwt.issuer'),
         'aud' => getenv('jwt.audience'),
         'iat' => $issuedAt,
+        'nbf' => $issuedAt - 60,
         'exp' => $expire,
-        'sub' => $user['id'],
+        'sub' => (string)$user['id'],
         'email' => $user['email'],
+        'jti' => bin2hex(random_bytes(16))
     ];
 
     return JWT::encode($payload, getenv('jwt.secret'), 'HS256');
 }
 
-function decodeJWT($token): Firebase\JWT\JWT|stdClass|string
+function decodeJWT(string $token): Firebase\JWT\JWT|stdClass|string
 {
     try {
         return JWT::decode($token, new Key(getenv('jwt.secret'), 'HS256'));
